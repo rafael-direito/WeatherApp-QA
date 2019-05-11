d398621ebb4f1b8f3e0fcf3eb3e5591a2885f585
Rafael Direito <rafael.neves.direito@ua.pt>
2019-05-12 00:53:28 +0100

    web interface updated - after sonarqube

--- web_interface/src/main/java/constants/Constants.java
+++ web_interface/src/main/java/constants/Constants.java
@@ -9,17 +9,13 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author rd
  */
 public class Constants
-{
-    public Constants(){};
-    
+{    
     public static final String BASE_API = "http://localhost:8081/";
     
     
